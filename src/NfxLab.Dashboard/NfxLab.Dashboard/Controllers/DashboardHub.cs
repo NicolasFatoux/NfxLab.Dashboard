39b9faa1fb5a2760474cbd5e4d7cbe4c1c25ed40
Nicolas Fatoux <nicolas.fatoux@gmail.com>
2015-03-04 23:41:20 +0100

    prototype

---
+++ src/NfxLab.Dashboard/NfxLab.Dashboard/Controllers/DashboardHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace NfxLab.Dashboard.Controllers
+{
+    public class DashboardHub : Hub
+    {
+    }
+}
---
+++ src/NfxLab.Dashboard/NfxLab.Dashboard/Controllers/SensorDataController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace NfxLab.Dashboard.Controllers
+{
+    public class SensorDataController : ApiController
+    {
+        public void Put(dynamic data)
+        {
+            HttpContext.Current.Application["Data"] = data;
+            HttpContext.Current.Application["UpdateDate"] = DateTime.Now;
+
+            var context = GlobalHost.ConnectionManager.GetHubContext<DashboardHub>();
+            context.Clients.All.updateData(data);
+        }
+    }
+}
