5d9e133101963db849cef3a9a4ad782c8b2fcc3e
Nicolas Fatoux <nicolas.fatoux@gmail.com>
2015-03-07 20:24:34 +0100

    js cleanup

--- src/NfxLab.Dashboard/NfxLab.Dashboard/Scripts/dashboard.js
+++ src/NfxLab.Dashboard/NfxLab.Dashboard/Scripts/dashboard.js
@@ -9,28 +9,5 @@ $(function () {
         $("#updateDate").text(data.UpdateDate);
     };
 
-    $.connection.hub.start().done(function () {
-        // Wire up Send button to call NewContosoChatMessage on the server.
-        //$('#newContosoChatMessage').click(function () {
-        //    contosoChatHubProxy.server.newContosoChatMessage($('#displayname').val(), $('#message').val());
-        //    $('#message').val('').focus();
-        //});
-
-        //$("#debugPut").click(function () {
-        //    var data = {
-        //        temperature: $("#debugTemperature").val(),
-        //        humidity: $("#debugHumidity").val()
-        //    };
-
-        //    $.ajax({
-        //        url: '/api/SensorData',
-        //        type: 'PUT',
-        //        contentType: 'application/json',
-        //        data: JSON.stringify(data),
-        //        success: function (result) {
-        //            // Do something with the result
-        //        }
-        //    });
-        //})
-    });
+    $.connection.hub.start();
 });
